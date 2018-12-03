aa0f8cba6a1d17a068ea5d48790f1569a12174dc
lvtoxic <toxic675381013@gamil.com>
2018-12-03 14:51:11 +0800

    c

--- XX/unittest/D11/excelTest.py
+++ XX/unittest/D11/excelTest.py
@@ -1,10 +1,20 @@
 #coding=utf-8
 
+import xlrd
+import os
 
+def base_dir(filename=None):
+    return os.path.join(os.path.dirname(r'C:\Users\Administrator\Desktop\name3.xlsx'),filename)
 
+work=xlrd.open_workbook(base_dir('name3.xlsx'))
+# sheet=work.sheet_by_index(0)
+sheet=work.sheet_by_name('Sheet1')
 
+#查看多少行
+print(sheet.nrows)
 
-
+#获取单元格内容(行,列)
+print(sheet.cell_value(4,0))
 
 
 
--- XX/unittest/D11/f4.py
+++ XX/unittest/D11/f4.py
@@ -1,1 +1,6 @@
-#coding=utf-8
+# coding=utf-8
+
+import requests
+'''有安全证书提示ecrt或443的时候用cerify=false可以忽略'''
+r = requests.get(url='https://www.12306.cn/mormhweb', verify=False)
+print(r.content.decode('utf-8'))
